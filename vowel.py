924408f00471e9f20c2fc4eed9951c2c516f89bb
mhussain <imuhammadhussain003@gmail.com>
2024-07-22 15:08:00 +0500

    First comit

---
+++ vowel.py
@@ -0,0 +1,5 @@
+a=input("Enter an alphabet: ")
+if a=="a"or a=="e" or a=="i" or a=="o" or a=="u":
+    print("The alphabet is vowel")
+else:
+    print("the alphabet is not vowel")
---
+++ word.py
@@ -0,0 +1,5 @@
+tuple1="ankish",10,30
+n1,n2,n3=tuple1
+print(n1)
+print(n2)
+print(n3)
