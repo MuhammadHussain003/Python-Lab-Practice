b2221b85292b1e1e76340a18475327a3bdd9fa7d
MuhammadHussain003 <imuhammadhussain003@gmail.com>
2024-07-23 12:58:15 +0500

    Encapsulation solution

---
+++ Overloading/Q#31.py
@@ -0,0 +1,17 @@
+
+ class MathOperation:
+    # def __init__(self,a,b):
+    #     self.a=a
+    #     self.b=b
+    a=int(input("Enter first value"))
+    b=int(input("Enter second value"))
+    def Addition(self,int ):
+        print("Addition of two integer is",self.a+self.b)
+    def Addition(self,float):
+        print("Addition of two floating value is", self.a + self.b)
+
+    # def Addition(self,list):
+    #     print("Addition of two list is", self.a + self.b)
+
+m = MathOperation()
+m.Addition()
